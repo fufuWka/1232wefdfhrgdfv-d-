2ec02fd439b631a8806c8799865ea47eb61761ad
fufuWka <sashadpvp@gmail.com>
2026-07-13 16:36:09 +0400

    Add configuration for bot and subscription details

--- config.py
+++ config.py
@@ -1,20 +1,28 @@
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
+# ==========================
+# Telegram
+# ==========================
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
 
 ADMIN_ID = int(
     os.getenv(
         "ADMIN_ID",
-        0
+        "0"
     )
 )
 
+BOT_NAME = "NE_FREE_VPN_bot"
+
+BOT_LINK = "https://t.me/NE_FREE_VPN_bot"
+
+# ==========================
+# GitHub Gist
+# ==========================
 
 FREE_SUB = (
     "https://gist.githubusercontent.com/"
@@ -23,7 +31,6 @@ FREE_SUB = (
     "FREE_VPN.bot"
 )
 
-
 PRO_SUB = (
     "https://gist.githubusercontent.com/"
     "fufuWka/58e04a989aea6e1adaf6fe809525148f/"
@@ -31,5 +38,29 @@ PRO_SUB = (
     "PRO_VPN.bot"
 )
 
+# ==========================
+# Тарифы
+# ==========================
 
-BOT_NAME = "NE_FREE_VPN_bot"
+FREE_NAME = "FREE"
+FREE_SERVERS = 3
+
+PRO_NAME = "PRO"
+PRO_SERVERS = 8
+
+# ==========================
+# Поддержка
+# ==========================
+
+SUPPORT_TEXT = (
+    "Если возникли проблемы с VPN,\n"
+    "обновите подписку.\n\n"
+    "Если проблема сохраняется,\n"
+    "обратитесь в поддержку."
+)
+
+# ==========================
+# Версия
+# ==========================
+
+BOT_VERSION = "v3.0"
